d4cb03221744d27c4d8c2b9a91aaf18c632c1800
bhagyshricompany <116645353+bhagyshricompany@users.noreply.github.com>
2024-04-03 00:55:29 +0530

    createpgaesandroutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/profile'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+   <BrowserRouter>
+   <Routes>
+   <Route path="/" element={<Home />} />
+  
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile/>} />
+    <Route path="/signin" element={<SignIn />} />
+    <Route path="/signout" element={<SignOut />} />
+    </Routes>
+
+   </BrowserRouter>
   )
 }
 
